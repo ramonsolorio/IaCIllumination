1098fcd8a905e2b45c8fa94fe56561e7ba89ed26
Ramon Solorio Fernandez <ramons@microsoft.com>
2024-06-07 15:54:16 -0600

    Updates

---
+++ modules/azure/storage-account/variables.tf
@@ -0,0 +1,31 @@
+#Define storage account variables  
+variable "storage_account_name" {
+  description = "The name of the storage account"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create the storage account"
+  type        = string
+}
+
+variable "location" {
+  description = "The location/region in which to create the storage account"
+  type        = string
+}
+
+variable "account_tier" {
+  description = "Defines the Tier to use for this storage account"
+  type        = string
+}
+
+variable "account_replication_type" {
+  description = "Defines the type of replication to use for this storage account"
+  type        = string
+}
+
+variable "tags" {
+  description = "The tags to associate with the storage account"
+  type        = map(string)
+  default     = {}
+}
