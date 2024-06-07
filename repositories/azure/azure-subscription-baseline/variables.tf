5617e83473dc92f847756f2519a861227a0a7d72
Ramon Solorio Fernandez <ramons@microsoft.com>
2024-06-07 15:45:34 -0600

    Updates

--- repositories/azure/azure-subscription-baseline/variables.tf
+++ repositories/azure/azure-subscription-baseline/variables.tf
@@ -1,10 +1,12 @@
+#=================global=================
+variable "tags" {}
+
 #========================resource group========================
 variable "group_name" {}
 variable "location" {}
-variable "tags" {}
+
 
 #========================storage account========================
 variable "storage_account_name" {}
 variable "account_tier" {}
 variable "account_replication_type" {}
-variable "tags" {}
